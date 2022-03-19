929c009de4e7488c2dae25403fe0075c807523ea
Jay <1598353326@qq.com>
2022-03-19 23:43:55 +0800

    🚀 perf: test perf

--- .releaserc.js
+++ .releaserc.js
@@ -29,17 +29,17 @@ module.exports = {
         preset: "emoji-config",
         presetConfig: { // conventional changelog config https://github.com/conventional-changelog/conventional-changelog-config-spec
           "types": [ // 需要和 .cz-config.js 、changlogrc.js 的 type 保持一致
-            { "type": "✨ feat", "section": "✨ Features | 新功能" },
-            { "type": "🔧 fix", "section": "🛠️ Bug Fixes | Bug 修复" },
-            { "type": "📝 docs", "section": "📝 Documentation | 文档" },
-            { "type": "🎨 style", "section": "🎨 Styles | 风格" },
-            { "type": "♻️ refactor", "section": "♻️ Code Refactoring | 代码重构" },
-            { "type": "🚀 perf", "section": "🚀 Performance Improvements | 性能优化" },
-            { "type": "🧪 test", "section": "📸 Tests | 测试" },
-            { "type": "📦‍ build", "section": "📦‍ Build System | 打包构建" },
-            { "type": "👷 ci", "section": "🐳 Continuous Integration | CI 配置" },
-            { "type": "🍻 chore", "section": "🍻 chore | 构建/工程依赖/工具" },
-            { "type": "💊 revert", "section": "💊 Revert | 回退" }
+            { "type": "feat", "section": "✨ Features | 新功能" },
+            { "type": "fix", "section": "🛠️ Bug Fixes | Bug 修复" },
+            { "type": "docs", "section": "📝 Documentation | 文档" },
+            { "type": "style", "section": "🎨 Styles | 风格" },
+            { "type": "refactor", "section": "♻️ Code Refactoring | 代码重构" },
+            { "type": "perf", "section": "🚀 Performance | 性能优化" },
+            { "type": "test", "section": "📸 Tests | 测试" },
+            { "type": "build", "section": "📦‍ Build System | 打包构建" },
+            { "type": "ci", "section": "🐳 Continuous Integration | CI 配置" },
+            { "type": "chore", "section": "🍮 Chore | 构建/工程依赖/工具" },
+            { "type": "revert", "section": "💊 Revert | 回退" }
           ]
         }
       }
